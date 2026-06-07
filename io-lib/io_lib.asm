3b0f610bfe5d94d64801eb68ddccad138a177d71
rivon0507 <flavienrivon@gmail.com>
2026-06-07 15:26:23 +0300

    refine read_word, and implement parse_int and parse_uint

--- io-lib/io_lib.asm
+++ io-lib/io_lib.asm
@@ -7,6 +7,8 @@ global print_uint
 global print_int
 global read_char
 global read_word
+global parse_uint
+global parse_int
 
 section .text
 exit:
@@ -52,7 +54,7 @@ print_uint:
     push rbx
     mov rbp, rsp
     sub rsp, 0x20
-    and rbx, 0x1
+    xor rbx, rbx
     mov rax, rdi
     mov rcx, 0xa
     lea rsi, [rbp - 0x8]
@@ -114,84 +116,125 @@ test_whitespace:
 
 read_word:
     push rbx
+    push r12
+    push r13
     xor rbx, rbx
-    push rdi
-    push rsi
+    mov r12, rdi
+    mov r13, rsi
 
     mov rdx, rsi
     mov rsi, rdi
     mov rdi, 0
     mov rax, 0
     syscall
+    cmp rax, 0x0
+    jl .read_error
     add rbx, rax
-
-    pop rsi
-    pop rdi
     xor rcx, rcx
 .find_offset_loop:
     cmp rcx, rax
-    jg .epilogue
-    push rdi
-    mov dil, [rdi + rcx]
+    jge .all_whitespace
+    mov dil, [r12 + rcx]
     call test_whitespace
-    pop rdi
     jne .found_offset
     inc rcx
     jmp .find_offset_loop
+.all_whitespace:
+    xor rbx, rbx
+    jmp .no_more_offset
 .found_offset:
-    push rsi
     test rcx, rcx
-    jz .no_offset
+    jz .epilogue
     mov rdx, rcx
     sub rbx, rcx
+    lea rsi, [r12 + rdx]
     xor rcx, rcx
 .trim_left:
-    add rdi, rdx
-    mov sil, byte [rdi + rcx]
-    sub rdi, rdx
-    mov [rdi + rcx], sil
-    inc rcx
-    cmp rcx, rax
+    cmp rcx, rbx
     jge .no_more_offset
+    mov dil, byte [rsi + rcx]
+    mov [r12 + rcx], dil
+    inc rcx
     jmp .trim_left
 .no_more_offset:
-    pop rsi
-    push rdi
-    sub rsi, rax
-    add rsi, rdx
-    add rdi, rax
-    sub rdi, rdx
-
-    mov rdx, rsi
-    mov rsi, rdi
-    mov rdi, 0
     mov rax, 0
+    mov rdi, 0
+    lea rsi, [r12 + rbx]
+    mov rdx, r13
+    sub rdx, rbx
     syscall
+    cmp rax, 0x0
+    jl .read_error
     add rbx, rax
-
-    pop rdi
-    jmp .epilogue
-.no_offset:
-    pop rsi
 .epilogue:
     xor rcx, rcx
-    mov rdx, rdi
 .find_null_location:
-    cmp rcx, rsi
+    cmp rcx, r13
     jge .not_fitting
     cmp rcx, rbx
     jge .add_null_term
-    mov dil, [rdx + rcx]
+    mov dil, [r12 + rcx]
     call test_whitespace
     je .add_null_term
     inc rcx
     jmp .find_null_location
 .add_null_term:
-    mov byte [rdx + rcx], 0x0
-    mov rax, rdx
+    mov byte [r12 + rcx], 0x0
+    mov rax, r12
     jmp .end
+.read_error:
 .not_fitting:
     mov rax, 0
 .end:
+    pop r13
+    pop r12
     pop rbx
     ret
+
+parse_uint:
+    ; rdi: null-terminated string to parse an uint from
+    ; rbx: parsed number
+    ; rax: parsed characters count
+    push rbx
+    mov rbx, 0xa
+    xor rcx, rcx
+    xor rax, rax
+    xor rsi, rsi
+.parse_char:
+    mov sil, byte [rdi + rcx]
+    sub sil, 0x30
+    cmp sil, 0x0
+    jl .end_of_number
+    cmp sil, 0x9
+    jg .end_of_number
+    mul rbx
+    add rax, rsi
+    inc rcx
+    jmp .parse_char
+.end_of_number:
+    mov rdx, rcx
+    pop rbx
+    ret
+
+parse_int:
+    ; rdi: null-terminated string to parse an int from
+    ; rbx: parsed number
+    ; rax: parsed characters count
+    ; rbx: the sign, 0 for positive, 1 for negative
+    push rbx
+    push rdi
+    xor rbx, rbx
+    cmp byte [rdi], 0x2d
+    jne .positive
+    mov rbx, 0x1
+    lea rdi, [rdi + 1]
+.positive:
+    call parse_uint
+    test rbx, rbx
+    jz .end ; skip signing
+    neg rax
+.end:
+    pop rdi
+    pop rbx
+    ret
+
--- io-lib/test_io_lib.asm
+++ io-lib/test_io_lib.asm
@@ -7,43 +7,136 @@ extern print_uint
 extern print_int
 extern read_char
 extern read_word
+extern parse_uint
+extern parse_int
 
 section .data
-hello: db 'Hello, world!', 0
+hello: db "Hello, world!", 0
+t_print_char: db "[print_char]", 0xa, 0
+t_print_uint: db "[print_uint]", 0xa, 0
+t_print_int: db "[print_int]", 0xa, 0
+t_read_char: db "[read_char] Enter a character>", 0
+t_ascii_of_your_char: db "Ascii of your char: ", 0 
+t_enter_a_word: db "[Enter a word]>", 0
+t_no_word_read: db "[NO WORD READ]", 0xa, 0
+t_your_number_is: db "[Your number is] ", 0
+t_parsed_chars: db " [Parsed chars] ", 0
+t_your_word_is: db "[Your word is] ", 0
+t_enter_a_number: db "[Enter a number]>", 0 
+t_enter_a_positive_number: db "[Enter a positive number]>", 0 
 
 section .text
 
 _start:
-    mov rdi, hello
-    call print_string
+    push rbx
+    push r12
+    lea rdi, [rel hello]
+    call print_string ; Hello, world!
     call print_newline
-    mov rdi, [hello + 0x2]
-    call print_char
+    lea rdi, [rel t_print_char]
+    call print_string ; [print_char]
+    mov rdi, [rel hello + 0x2]
+    call print_char ; H
     call print_newline
+    lea rdi, [rel t_print_uint]
+    call print_string ; [print_uint]
     mov qword rdi, 123488
     call print_uint
     call print_newline
+    lea rdi, [rel t_print_uint]
+    call print_string ; [print_uint] 
     mov qword rdi, -123488
     call print_uint
     call print_newline
+    lea rdi, [rel t_print_int]
+    call print_string ; [print_int] 
     mov qword rdi, -123488
     call print_int
     call print_newline
+    lea rdi, [rel t_read_char]
+    call print_string ; [read_char] Enter a character: 
     call read_char
-    mov rdi, rax
+    mov rbx, rax
+    lea rdi, [rel t_ascii_of_your_char]
+    call print_string ; Ascii of your char:  
+    mov rdi, rbx
     call print_uint
     call print_newline
 
     push rbp
     mov rbp, rsp
     sub rsp, 0x20
+    lea rdi, [rel t_enter_a_word]
+    call print_string ; [Enter a word] 
     lea rdi, [rsp]
     mov rsi, 0xa
     call read_word
-    mov rdi, rax
-    call print_string
+    test rax, rax
+    jz .no_word_read
+    mov rbx, rax
+    lea rdi, [rel t_your_word_is]
+    call print_string ; Your word is:  
+    mov rdi, rbx
+    call print_string 
     call print_newline
+    jmp .read_uint
+.no_word_read:
+    lea rdi, [rel t_no_word_read]
+    call print_string ; [NO WORD READ]
+.read_uint:
+    lea rdi, [rel t_enter_a_positive_number]
+    call print_string ; [Enter a positive number] 
+    lea rdi, [rsp]
+    mov rsi, 0x10
+    call read_word
+    test rax, rax
+    jz .no_word_read_2
+    mov rdi, rax
+    call parse_uint
+    mov rbx, rax
+    mov r12, rdx
+    lea rdi, [rel t_your_number_is]
+    call print_string ; [Your number is] 
+    mov rdi, rbx
+    call print_uint
+    lea rdi, [rel t_parsed_chars] ;  [Parsed chars] 
+    call print_string
+    mov rdi, r12
+    call print_uint
+    call print_newline
+    jmp .read_int
+.no_word_read_2:
+    lea rdi, [rel t_no_word_read]
+    call print_string ; [NO WORD READ]
+.read_int:
+    lea rdi, [rel t_enter_a_number]
+    call print_string ; [Enter a number] 
+    lea rdi, [rsp]
+    mov rsi, 0x10
+    call read_word
+    test rax, rax
+    jz .no_word_read_3
+    mov rdi, rax
+    call parse_int
+    mov rbx, rax
+    mov r12, rdx
+    lea rdi, [rel t_your_number_is]
+    call print_string ; [Your number is] 
+    mov rdi, rbx
+    call print_int
+    lea rdi, [rel t_parsed_chars] ;  [Parsed chars] 
+    call print_string
+    mov rdi, r12
+    call print_uint
+    call print_newline
+    jmp .end
+.no_word_read_3:
+    lea rdi, [rel t_no_word_read]
+    call print_string ; [NO WORD READ]
+.end:
     mov rsp, rbp
     pop rbp
+    pop r12
+    pop rbx
     mov rdi, 0
     call exit
